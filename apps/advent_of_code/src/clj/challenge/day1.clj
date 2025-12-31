2aa7e491d97a1c827abe0c19670282c16310d95d
Gary Paige <argvader@gmail.com>
2025-12-31 08:53:32 -0500

    advent day1 2025

---
+++ apps/advent_of_code/src/clj/challenge/day1.clj
@@ -0,0 +1,44 @@
+(ns challenge.day1
+  (:require [clojure.string :as str]))
+
+(def starting-point "R50")
+(def dial-size 100)
+
+(defn left-right 
+   [rotate]
+   (let [dir (first rotate)
+         amount (Integer/parseInt (subs rotate 1))]
+     (if (= \L dir)
+         (- amount)
+         amount)))
+
+(defn dial-mod [current rotation]
+  (mod (+ current rotation dial-size) dial-size))    
+
+(defn rotate-dial
+  [rotations]
+  (loop [current 0
+         rotations-left rotations
+         positions []]
+    (if (empty? rotations-left)
+       positions
+       (let [rotation (left-right (first rotations-left))
+             new-pos (dial-mod current rotation)]
+         (recur new-pos (rest rotations-left) (conj positions new-pos))))))
+
+(defn count-zero [positions]
+  (->> positions
+       (filter #(= % 0))
+       (count)))
+             
+(defn -main
+  [& args]
+  (println "Finding Password")
+  (-> (first args)
+      (slurp)
+      (str/split-lines)
+      (seq)
+      (conj starting-point)
+      (rotate-dial)
+      (count-zero)
+      (println)))
